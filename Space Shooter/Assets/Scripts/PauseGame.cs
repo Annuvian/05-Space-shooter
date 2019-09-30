7b206b936d803c67cdb7f1513df4361ac0541ad0
rocfrank <rocfrank@iu.edu>
2019-09-30 14:24:04 -0400

    Adds some scripts

---
+++ Space Shooter/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public static void Pause()
+    {
+        if (Time.timeScale > 0)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
